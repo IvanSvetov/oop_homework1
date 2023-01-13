8f78eff8ee1dbabd96e769ba6bb549a1b07fbd01
Ivan Svetov <ivan_lbov@mail.ru>
2023-01-13 21:13:15 +0500

    Homework is done, check it.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
+import transport.Car;
+
 public class Main {
     public static void main(String[] args) {
 
-        Car car1 = new Car("Lada", "Granta", 1.7,"Yellow", 2015, "Russia");
-        Car car2 = new Car("Audi", "A8 50 L TDI quattro", 3.0,"Black", 2020, "Germany");
-        Car car3 = new Car("BMW", "Z8", 3.0,"Black", 2021, "Germany");
-        Car car4 = new Car("Kia", "Sportage 4 generation", 2.4,"Red", 2018, "South Korea");
-        Car car5 = new Car("Hyundai", "Avante", 1.6,"Orange", 2016, "Germany");
+        Car car1 = new Car("Lada", "Granta", 1.7,"Yellow", 2015, "Russia", "MT", "hachback", "111", 5);
+        Car car2 = new Car("Audi", "A8 50 L TDI quattro", 3.0,"Black", 2020, "Germany", "MT", "hachback", "112", 5);
+        Car car3 = new Car("BMW", "Z8", 3.0,"Black", 2021, "Germany", "AT", "hachback", "113", 2);
+        Car car4 = new Car("Kia", "Sportage 4 generation", 2.4,"Red", 2018, "South Korea","AT", "hachback", "113",5);
+        Car car5 = new Car("Hyundai", "Avante", 1.6,"Orange", 2016, "Germany","MT", "sedan", "115", 4);
+
         System.out.println(car1);
         System.out.println(car2);
         System.out.println(car3);
         System.out.println(car4);
         System.out.println(car5);
+
+        car1.isWinterTires();
+        car2.isWinterTires();
+        car1.setMonth(6);
+        car1.isWinterTires();
     }
 }
---
+++ src/transport/Car.java
@@ -0,0 +1,100 @@
+package transport;
+
+public class Car {
+    final String brand;
+    final String model;
+    double engineVolume;
+    String color;
+    final int year;
+    final String country;
+    String gearBox;
+    final String bodyType;
+    String carReg;
+    final int numberOfSeats;
+    boolean winterTires;
+    public int month = 10;
+
+    public Car(String brand, String model, double engineVolume, String color, int year, String country, String gearBox, String bodyType, String carReg, int numberOfSeats) {
+        this.brand = brand == null ? "default" : brand;
+        this.model = model == null || model.isBlank() ? "default" : model;
+        this.engineVolume = engineVolume > 0 ? engineVolume : 1.5;
+        this.color = color == null || color.isBlank() ? "white" : color;
+        this.year = year > 0 ? year : 2000;
+        this.country = country == null || country.isBlank() ? "default" : country;
+        this.gearBox = gearBox == null || gearBox.isBlank() ? "default" : gearBox;
+        this.carReg = carReg == null || carReg.isBlank() ? "default" : carReg;
+        this.bodyType = bodyType == null || bodyType.isBlank() ? "default" : bodyType;
+        this.numberOfSeats = numberOfSeats <=0 ? 4 : numberOfSeats;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String getGearBox() {
+        return gearBox;
+    }
+    public void setGearBox(String gearBox) {
+        this.gearBox = gearBox;
+    }
+    public String getBodyType() {
+        return bodyType;
+    }
+    public String getCarReg() {
+        return carReg;
+    }
+    public void setCarReg(String carReg) {
+        this.carReg = carReg;
+    }
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+    public boolean getWinterTires() { return winterTires; }
+    public int getYear() {
+        return year;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public int getMonth() {
+        return month;
+    }
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    @Override
+    public String toString() {
+        return "brand = " + brand + ", model = " + model + ", engineVolume=" + engineVolume +
+                ", color = " + color + ", year = " + year + ", country = " + country + ", gearBox = " + gearBox +
+                ", bodyType = " + bodyType + ", carReg = " + carReg + ", numberOfSeats = " + numberOfSeats;
+    }
+    public class Key {
+        private final boolean remoteEngineStart = true;
+        private final boolean keyLessAccess = true;
+        Key key = new Key(remoteEngineStart, keyLessAccess);
+        public Key(boolean remoteEngineStart, boolean keyLessAccess) {
+        }
+    }
+    public boolean isWinterTires() {
+        if (month <= 4 || month >= 9) {
+            System.out.println("Winter tires installed");
+    }   else
+            System.out.println("Summer tires installed");
+        return winterTires;
+    }
+
+}
