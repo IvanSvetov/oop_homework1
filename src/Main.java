70b92d0fb4f27e27803041ab501e1859da3b5f14
Ivan Svetov <ivan_lbov@mail.ru>
2023-02-01 21:39:25 +0500

    Homework is done, check it.

--- src/Main.java
+++ src/Main.java
@@ -15,24 +15,3 @@ public class Main {
 
     }
 }
-
-
-
-//        Car car1 = new Car("Ferrari", "F12 Berlinetta", 6.3);
-//        Car car2 = new Car("Pagani", "Huayra", 6.0);
-//        Car car3 = new Car("Lamborghini ", "Aventador", 6.5);
-//        Car car4 = new Car("McLaren ", "MP4-12C", 3.8);
-//
-//        Truck truck1 = new Truck("Volvo", "Wild Viking", 12.8);
-//        Truck truck2 = new Truck("Scania", "Red Pearl R999", 15.6);
-//        Truck truck3 = new Truck("Tata", "Prima Race Truck", 12.0);
-//        Truck truck4 = new Truck("Freightliner", "Cascadia Banks Super-Turbo", 8.3);
-//
-//        Bus bus1 = new Bus("MAZ", "251", 10.5);
-//        Bus bus2 = new Bus("Yutong", "zk6129h", 8.8);
-//        Bus bus3 = new Bus("Ikarus", "386", 10.0);
-//        Bus bus4 = new Bus("Kia", "Grandbird", 12.9);
-//
-//        DriverCatB driverCatB = new DriverCatB("Sebastian Vettel", true, 16);
-//        DriverCatC driverCatC = new DriverCatC("Lewis Carl Davidson Hamilton", true, 13);
-//        DriverCatD driverCatD = new DriverCatD(" Fernando Alonso", true, 23);
